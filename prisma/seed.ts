c6c9b7a8ad0fb2d0a1c34c8bc9d86d5040474266
umutdiyar <umutdiyarbalci@gmail.com>
2026-06-06 02:25:48 +0300

    Use standard Prisma client

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,8 +2,7 @@ import "dotenv/config";
 
 import bcrypt from "bcryptjs";
 
-import { PrismaClient } from "../src/generated/prisma/client";
-
+import { PrismaClient } from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 
 const adapter = new PrismaPg({
